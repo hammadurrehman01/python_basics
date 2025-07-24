2e8148376e69ee53ecc19e8cc64e11e555de11a8
hammadurrehman-dev <hammad.desopers@gmail.com>
2025-07-24 16:52:23 +0500

    Add examples for set, frozenset, and byte data handling; read image file

--- index.py
+++ index.py
@@ -27,5 +27,19 @@
 # cat = Cat()
 # print(cat.speak())
 
+# my_set = {1, 2, "Hammad", 4, 5}
+# print(my_set)
 
- 
+# frozen_set = frozenset([1, 2, 3])
+# print(frozen_set)
+
+
+
+# byte_data = b"Hello"
+# print(byte_data)
+
+
+with open("https://images.pexels.com/photos/32262510/pexels-photo-32262510.jpeg", "rb") as image_file:
+    image_data = image_file.read()
+
+print(image_file)
