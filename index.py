3011a181b4571564b3d02079867dfd370c8ca669
hammadurrehman01 <hammadurrehman@techlabs.cc>
2025-07-24 06:56:46 +0500

    Add commented-out code for greeting and animal classes

--- index.py
+++ index.py
@@ -1,2 +1,31 @@
-from hello import sum
-sum(3, 2)
+# def greet(name):
+#   return f"Hello, {name}!"
+
+# def call_function(func, name):
+#   return func(name)
+
+# print(call_function(greet, "Hammad"))
+
+
+
+# class Animal:
+#     def speak(self):
+#         return "Animal Speaks!"
+    
+# class Dog():
+#     def speak(self):
+#         return "Woof!"
+    
+# class Cat():
+#     def speak(self):
+#         return "Meow!"
+    
+    
+# dog = Dog()
+# print(dog.speak())
+
+# cat = Cat()
+# print(cat.speak())
+
+
+ 
